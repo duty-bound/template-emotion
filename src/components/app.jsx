23d792fb5f3344a9b17db675ddc39e35ecc73501
duty-bound <duty_bound@aol.com>
2022-08-04 20:27:06 +0200

    First commit

---
+++ src/components/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+// NEXT:  https://developers.artsy.net/v2/start?id=601da039ce263c000e656d0d
+
+export const App = () =>
+  <h1>Template</h1>
+  
---
+++ src/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { ThemeProvider, Global, css } from '@emotion/react'
+import { globalCSS } from './styles/global-css'
+import { theme } from './styles/theme'
+import { App } from './components/app'
+
+const mountNode = document.getElementById('mountNode')
+
+ReactDOM.render(
+  <>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+    <Global
+      styles={css`
+          ${globalCSS}
+      `}
+    />
+  </>,
+  mountNode,
+)
